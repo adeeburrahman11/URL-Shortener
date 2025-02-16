33208836c967474aeb0c22f58b414e292977708b
adeeburrahman11 <adeeburrahman9021@gmail.com>
2025-02-16 19:49:43 +0530

    login logic

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,5 +1,106 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { PulseLoader } from "react-spinners";
+import Error from "./Error";
+import { useState } from "react";
+import * as Yup from "yup";
+import UseFetch from "@/hooks/UseFetch";
+import { login } from "@/db/apiAuth";
+import { useEffect } from "react";
+
 const LogIn = () => {
-  return <div>LogIn</div>;
+  const [errors, setErrors] = useState([]);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const { data, loading, error, fn: fnLogin } = UseFetch(login, formData);
+
+  useEffect(() => {
+    console.log(data);
+    // if (error === null && data) {
+    // }
+  }, [data, error]);
+
+  const handleLogin = async () => {
+    setErrors([]);
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email("Invalid Email")
+          .required("Email is Required"),
+        password: Yup.string()
+          .min(6, "Password must be atleast 6 characters")
+          .required("Password is Required"),
+      });
+
+      await schema.validate(formData, { abortEarly: false });
+
+      await fnLogin();
+    } catch (e) {
+      const newErrors = {};
+
+      e?.inner?.forEach((err) => {
+        newErrors[err.path] = err.message;
+      });
+
+      setErrors(newErrors);
+    }
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          to your account if you already have one
+        </CardDescription>
+        {error && <Error message={error.message} />}
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="space-y-1">
+          <Input
+            onChange={handleInputChange}
+            name="email"
+            type="email"
+            placeholder="Enter Email"
+          />
+          {errors.email && <Error message={errors.email} />}
+        </div>
+        <div className="space-y-1">
+          <Input
+            onChange={handleInputChange}
+            name="password"
+            type="password"
+            placeholder="Enter Password"
+          />
+          {errors.password && <Error message={errors.password} />}
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button onClick={handleLogin}>
+          {loading ? <PulseLoader size={10} color="#51a2ff" /> : "Login"}
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default LogIn;
